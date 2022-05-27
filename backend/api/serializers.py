9d9c1283f06c8a71010b8a02d8e8fcc93ef86fc2
Denis Ilyushin <to@denisilyushin.ru>
2022-05-28 00:36:08 +0300

    UPD Follow api viewset

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,3 +1,34 @@
 from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from recipes.models import Recipe
+from users.models import Follow
 
 User = get_user_model()
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField(source='author.id')
+    email = serializers.ReadOnlyField(source='author.email')
+    username = serializers.ReadOnlyField(source='author.username')
+    first_name = serializers.ReadOnlyField(source='author.first_name')
+    last_name = serializers.ReadOnlyField(source='author.last_name')
+    is_subscribed = serializers.SerializerMethodField()
+    # recipes = serializers.SerializerMethodField()
+    recipes_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Follow
+        fields = ('id', 'email', 'username', 'first_name', 'last_name',
+                  'is_subscribed', 'recipes_count')
+
+    def get_is_subscribed(self, obj):
+        return Follow.objects.filter(
+            user=obj.user, author=obj.author
+        ).exists()
+
+    # def get_recipes(self, obj):
+    #     return 10
+
+    def get_recipes_count(self, obj):
+        return Recipe.objects.filter(author=obj.author).count()
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,89 @@
 from django.contrib.auth import get_user_model
+from django.db import IntegrityError
 from djoser.views import UserViewSet as DjoserUserViewSet
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
+
+from users.models import Follow
+
+from api.serializers import FollowSerializer
 
 User = get_user_model()
 
 
 class UserViewSet(DjoserUserViewSet):
     pagination_class = LimitOffsetPagination
+
+    @action(
+        methods=('POST',),
+        detail=True,
+        permission_classes=(IsAuthenticated,)
+    )
+    def subscribe(self, request, id=None):
+        try:
+            follow = Follow.objects.create(
+                user=request.user,
+                author=get_object_or_404(User, id=id)
+            )
+        except IntegrityError:
+            return Response(
+                {'errors': (
+                    'Ошибка подписки. Нельзя подписываться '
+                    'повторно или на самого себя.'
+                )},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        serializer = FollowSerializer(follow, context={'request': request})
+        return Response(
+            serializer.data,
+            status=status.HTTP_201_CREATED
+        )
+
+    @subscribe.mapping.delete
+    def del_subscribe(self, request, id=None):
+        try:
+            Follow.objects.filter(
+                user=request.user,
+                author=get_object_or_404(User, id=id)
+            ).delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        except Exception as e:
+            return Response(
+                {'errors': (
+                    f'Ошибка отписки. Подписки не существует или вы уже '
+                    f'отписались.',
+                )},
+                status=status.HTTP_400_BAD_REQUEST)
+
+        # user = request.user
+        # author = get_object_or_404(User, id=id)
+        # if user == author:
+        #     return Response(
+        #         {'errors': (
+        #             'Ошибка отписки. Нельзя отписаться от самого себя.'
+        #         )},
+        #         status=status.HTTP_400_BAD_REQUEST)
+        # follow = Follow.objects.filter(user=user, author=author)
+        # if follow.exists():
+        #     follow.delete()
+        #     return Response(status=status.HTTP_204_NO_CONTENT)
+        # return Response(
+        #     {'errors': (
+        #             'Ошибка отписки. Подписки не существует или вы уже '
+        #             'отписались.'
+        #         )},
+        #     status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, permission_classes=(IsAuthenticated,))
+    def subscriptions(self, request):
+        serializer = FollowSerializer(
+            self.paginate_queryset(Follow.objects.filter(user=request.user)),
+            many=True,
+            context={'request': request}
+        )
+        return self.get_paginated_response(serializer.data)
