42271f779ca472c7fca29cbd9e7cba51f4795b08
Денис Илюшин <to@denisilyushin.ru>
2022-06-14 17:26:22 +0300

    DEPLOY. New

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('api/', include('api.urls')),
 ]
 
-if not settings.DEBUG:
+if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL,
         document_root=settings.MEDIA_ROOT
